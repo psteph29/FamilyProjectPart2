fc48358c49e0dab7d581ed5183a9b1049100ab79
Paige Stephenson <paigestephenson29@gmail.com>
2023-03-28 08:48:52 -0600

    first commit

---
+++ FamilyProjectPart2/FamilyMembers.swift
@@ -0,0 +1,8 @@
+//
+//  FamilyMembers.swift
+//  FamilyProjectPart2
+//
+//  Created by Paige Stephenson on 3/27/23.
+//
+
+import Foundation
---
+++ FamilyProjectPart2/PersonalDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PersonalDetailsViewController.swift
+//  FamilyProjectPart2
+//
+//  Created by Paige Stephenson on 3/27/23.
+//
+
+import UIKit
+
+class PersonalDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
